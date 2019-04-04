f4ca84f3bce561832c3729fd39f49b0406fd41e2
wtravis923 <willie.travis@gmail.com>
2019-04-04 13:45:37 -0400

    Add project files.

---
+++ GameNight/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameNight.Startup))]
+namespace GameNight
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
